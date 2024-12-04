03eae338e82f8640eadaa9a7203a1ff5b9eb80e3
Luc Timmerman <luc.timmerman@afas.nl>
2024-12-04 23:35:53 +0100

    Small fixes

--- Day.cs
+++ Day.cs
@@ -7,9 +7,9 @@ public abstract class Day
     public abstract void Part1();
     public abstract void Part2();
 
-    protected string GetInput(string type = null) => File.ReadAllText($"inputs\\{GetType().Name}{type}.txt");
+    protected string GetInput(string? type = null) => File.ReadAllText($"inputs\\{GetType().Name}{type}.txt");
 
-    protected string[] GetInputLines(string type = null) => GetInput(type).Split("\r\n");
+    protected string[] GetInputLines(string? type = null) => GetInput(type).Split("\r\n");
 
     protected void Answer(object answer)
     {
