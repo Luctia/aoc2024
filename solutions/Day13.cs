2d59eb279bde196f8c0e8f6ccf07360eea0b361b
Luc Timmerman <LUTI@afasgroep.nl>
2025-06-17 17:30:39 +0200

    Day 13 part 1

--- solutions/Day13.cs
+++ solutions/Day13.cs
@@ -4,52 +4,102 @@ namespace aoc2024.solutions;
 
 public partial class Day13 : Day
 {
-    public override void Part1()
+  public override void Part1()
+  {
+    var machines = GetClawMachines();
+    long total = 0;
+    foreach (var machine in machines)
     {
-        var lines = GetInputLines();
-        List<List<string>> groups = [];
-        List<string> group = [];
-        foreach (var line in lines)
-        {
-            if (string.IsNullOrEmpty(line))
-            {
-                groups.Add(group);
-                group = [];
-            }
-            else
-            {
-                group.Add(line);
-            }
-        }
+      total += machine.GetCost();
+    }
+
+    Answer(total);
+  }
+
+  public override void Part2()
+  {
+    var machines = GetClawMachines();
+    long total = 0;
+    foreach (var machine in machines)
+    {
+      machine.ActivatePartB();
+      total += machine.GetCost();
+    }
+
+    Answer(total);
+  }
+
+  private HashSet<ClawMachine> GetClawMachines()
+  {
+    var lines = GetInputLines();
+    List<List<string>> groups = [];
+    List<string> group = [];
+    foreach (var line in lines)
+    {
+      if (string.IsNullOrEmpty(line))
+      {
         groups.Add(group);
-        var machines = groups.Select(g => new ClawMachine(g)).ToList();
+        group = [];
+      }
+      else
+      {
+        group.Add(line);
+      }
     }
 
-    public override void Part2()
+    groups.Add(group);
+    return groups.Select(g => new ClawMachine(g)).ToHashSet();
+  }
+
+  private partial class ClawMachine
+  {
+    public (int x, int y) ButtonA;
+    public (int x, int y) ButtonB;
+    public (long x, long y) Prize;
+
+    public ClawMachine(List<string> input)
     {
-        throw new NotImplementedException();
+      ButtonA = ExtractNumbers(input[0]);
+      ButtonB = ExtractNumbers(input[1]);
+      Prize = ExtractNumbers(input[2]);
     }
 
-    private partial class ClawMachine
+    private static (int, int) ExtractNumbers(string line)
     {
-        public (int x, int y) ButtonA;
-        public (int x, int y) ButtonB;
-        public (int x, int y) Prize;
-
-        public ClawMachine(List<string> input)
-        {
-            ButtonA = ExtractNumbers(input[0]);
-            ButtonB = ExtractNumbers(input[1]);
-            Prize = ExtractNumbers(input[2]);
-        }
-
-        private static (int, int) ExtractNumbers(string line)
-        {
-            var numbers = MyRegex().Matches(line);
-            return (int.Parse(numbers[0].Groups[1].Value), int.Parse(numbers[0].Groups[2].Value));
-        }
-
-        [GeneratedRegex(@"^.*X.(\d+), Y.(\d+)$")]
-        private static partial Regex MyRegex();
+      var numbers = MyRegex().Matches(line);
+      return (int.Parse(numbers[0].Groups[1].Value), int.Parse(numbers[0].Groups[2].Value));
     }
+
+    public void ActivatePartB()
+    {
+      Prize.x += 10000000000000;
+      Prize.y += 10000000000000;
+    }
+
+    public long GetCost()
+    {
+      var aMax = Math.Max(Prize.x / ButtonA.x, Prize.y / ButtonA.y);
+      var bMax = Math.Max(Prize.x / ButtonB.x, Prize.y / ButtonB.y);
+      var candidates = new List<(long a, long b)>();
+      for (long a = 0; a < aMax; a++)
+      {
+        for (long b = 0; b < bMax; b++)
+        {
+          if (Prize.x == ButtonA.x * a + ButtonB.x * b && Prize.y == ButtonA.y * a + ButtonB.y * b)
+          {
+            candidates.Add((a, b));
+          }
+        }
+      }
+
+      if (candidates.Count == 0)
+      {
+        return 0;
+      }
+      return candidates.Select(c => 3 * c.a + c.b).Min();
+    }
+
+    [GeneratedRegex(@"^.*X.(\d+), Y.(\d+)$")]
+    private static partial Regex MyRegex();
+  }
 }
